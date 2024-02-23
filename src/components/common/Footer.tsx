4b89f5c1d8d6aea1a916e26f92e3c911d1bcc886
Jinwhan Kim <sodlfmag@gmail.com>
2024-02-23 10:59:39 +0900

    feat: 기본 레이아웃 구성 및 테마 설정

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <>
+      <hr />
+      <div>copyright(c) 2024. book store.</div>
+    </>
+  );
+}
+
+export default Footer;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Footer from "../components/common/Footer";
+import Header from "../components/common/Header";
+import { formatNumber } from "../uitls/format";
+
+const COUNT = 10000;
+
+function Home() {
+  return (
+    <>
+      <div>home body</div>
+    </>
+  );
+}
+
+export default Home;
